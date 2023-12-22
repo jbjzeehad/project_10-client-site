cb45c9f3d7d2178d9a10ce48cf6921b6eb24178b
jbjzeehad <ilamta.97@gmail.com>
2023-12-22 23:59:43 +0600

    used react rating

--- library-management-system/src/Component/Banner/Banner.jsx
+++ library-management-system/src/Component/Banner/Banner.jsx
@@ -1,34 +1,42 @@
-
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
 import './styles.css';
-
-// import required modules
 import { Pagination } from 'swiper/modules';
 
 const Banner = () => {
 
+    const buttons = <>
+        <button className="border-violet-500 border-2 rounded-lg m-3 p-3 hover:border-slate-300 text-xl hover:text-violet-500 ">Read More</button>
+    </>
+
     return (
-        <div>
+        <div className='relative h-full p-2'>
             <Swiper
                 slidesPerView={3}
-                spaceBetween={20}
+                spaceBetween={5}
                 pagination={{
                     clickable: true,
                 }}
                 modules={[Pagination]}
-                className="mySwiper"
+                className="mySwiper rounded-lg"
             >
-                <SwiperSlide><img src="https://i.ibb.co/PTDD9JZ/toyotacamry.png" alt="" /></SwiperSlide>
-                <SwiperSlide><img src="https://i.ibb.co/PTDD9JZ/toyotacamry.png" alt="" /></SwiperSlide>
-                <SwiperSlide><img src="https://i.ibb.co/PTDD9JZ/toyotacamry.png" alt="" /></SwiperSlide>
-                <SwiperSlide><img src="https://i.ibb.co/PTDD9JZ/toyotacamry.png" alt="" /></SwiperSlide>
-                <SwiperSlide><img src="https://i.ibb.co/PTDD9JZ/toyotacamry.png" alt="" /></SwiperSlide>
+
+                <SwiperSlide>
+
+                    <img className="relative rounded-lg" src="https://i.ibb.co/XScvv3S/horror1.png" />
+                    <div className="flex items-center justify-between absolute bottom-0 left-0 bg-slate-900 bg-opacity-70 py-5 px-5 w-full rounded-b-lg text-slate-300">
+                        <p className=' font-semibold text-3xl' >Ethel Cain</p>
+                        {buttons}
+                    </div>
+
+
+                </SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/VND3wv6/comic1.png" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/NKy6r9Y/history1.png" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/QDBKQsy/fiction1.png" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/XScvv3S/horror1.png" alt="" /></SwiperSlide>
+
             </Swiper>
 
 
